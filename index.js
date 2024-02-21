ca2ae8f09538bd83ee57074c59ded764b4903098
kirenskiysasha <kirenskiysasha@gmail.com>
2024-02-21 07:51:54 +0300

    third step

--- index.js
+++ index.js
@@ -10,58 +10,72 @@ this.startFunc = function () {
     lastName = document.forms[0]['secondName'].value;
     middleName = document.forms[0]['middleName'].value;
     comment = document.forms[0]['comment'].value;
-    checkbox = document.forms[0]['checkbox'].value;
+    checkbox = document.forms[0]['checkbox'].checked;
     console.log(email, phone, firstName, lastName, middleName, comment, checkbox)
     pay(email, phone, firstName, lastName, middleName, comment, checkbox)
 }
 
 this.pay = function (email, phone, firstName, lastName, middleName, comment, checkbox) {
     var widget = new cp.CloudPayments();
-       widget.pay('auth', // или 'charge'
-           { //options
-               publicId: 'pk_37c7e424d66b39e881cdb54bbfd1c', //id из личного кабинета
-               description: 'Оплата товаров', //назначение
-               amount: 100, //сумма
-               currency: 'RUB', //валюта
-               accountId: email, //идентификатор плательщика (необязательно)
-               invoiceId: '1234567', //номер заказа  (необязательно)
-               email: email, //email плательщика (необязательно)
-               skin: "mini", //дизайн виджета (необязательно)
-               autoClose: 3, //время в секундах до авто-закрытия виджета (необязательный)
-               data: {
-                   myProp: 'myProp value'
-               },
-               configuration: {
-                   common: {
-                       successRedirectUrl: "https://{ваш сайт}/success", // адреса для перенаправления 
-                       failRedirectUrl: "https://{ваш сайт}/fail"        // при оплате по Tinkoff Pay
-                   }
-               },
-               payer: { 
-                   firstName: firstName,
-                   lastName: lastName,
-                   middleName: middleName,
-                   birth: '1955-02-24',
-                   address: 'тестовый проезд дом тест',
-                   street: 'Lenina',
-                   city: 'MO',
-                   country: 'RU',
-                   phone: '123',
-                   postcode: '345'
-               }
-           },
-           {
-               onSuccess: function (options) { // success
-                   //действие при успешной оплате
-               },
-               onFail: function (reason, options) { // fail
-                   //действие при неуспешной оплате
-               },
-               onComplete: function (paymentResult, options) { //Вызывается как только виджет получает от api.cloudpayments ответ с результатом транзакции.
-                   //например вызов вашей аналитики
-               }
-           }
-       )
+
+    var data = {
+        email : email,
+        phone : phone,
+        firstName : firstName,
+        lastName : lastName,
+        middleName : middleName,
+        comment : comment
+    }
+
+    if (checkbox) { 
+        data.CloudPayments = {
+            recurrent: { interval: 'Month', period: 1 }
+        }
+    }
+
+    widget.pay('charge',
+        { //options
+            publicId: 'pk_37c7e424d66b39e881cdb54bbfd1c', //id из личного кабинета
+            description: 'Пожертвование в фонд', //назначение
+            amount: 100, //сумма
+            currency: 'RUB', //валюта
+            accountId: email, //идентификатор плательщика (необязательно)
+            invoiceId: '1234567', //номер заказа  (необязательно)
+            email: email, //email плательщика (необязательно)
+            skin: "mini", //дизайн виджета (необязательно)
+            autoClose: 3, //время в секундах до авто-закрытия виджета (необязательный)
+            data : data,
+            configuration: {
+                common: {
+                    successRedirectUrl: "https://{ваш сайт}/success", // адреса для перенаправления 
+                    failRedirectUrl: "https://{ваш сайт}/fail"        // при оплате по Tinkoff Pay
+                }
+            },
+            payer: { 
+                firstName: firstName,
+                lastName: lastName,
+                middleName: middleName,
+                birth: '1955-02-24',
+                address: 'тестовый проезд дом тест',
+                street: 'Lenina',
+                city: 'MO',
+                country: 'RU',
+                phone: '123',
+                postcode: '345'
+            }
+        },
+        {
+            onSuccess: function (options) { // success
+                //действие при успешной оплате
+            },
+            onFail: function (reason, options) { // fail
+                //действие при неуспешной оплате
+            },
+            onComplete: function (paymentResult, options) { //Вызывается как только виджет получает от api.cloudpayments ответ с результатом транзакции.
+                //например вызов вашей аналитики
+            }
+        }
+        )
    };
 
 Paymentbutton.onclick = startFunc
